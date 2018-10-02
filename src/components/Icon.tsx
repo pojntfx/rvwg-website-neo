191d7affe5cfa8b4a53181ceb7ab3de9518af0e2
Felix Pojtinger <felix@pojtinger.com>
2018-10-02 16:25:48 +0200

    feat(Navbar): add popovers with description and use only icons on mobile

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 
 const Icon = styled(FontAwesomeIcon)`
-  margin-right: 1rem;
+  margin-right: 0.5rem;
 `;
 
 export { Icon };
