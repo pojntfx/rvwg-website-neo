f22d6015dc9f4fb3f1e0b82415dc144ad5b51e6f
Felix Pojtinger <felix@pojtinger.com>
2018-10-02 21:56:14 +0200

    feat(Calendar): fetch data from Google calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Component } from "react";
 import BigCalendarTemplate from "react-big-calendar";
 import * as moment from "moment";
 import { Card } from "react-bootstrap";
@@ -10,16 +11,50 @@ const BigCalendar = styled(BigCalendarTemplate)`
   min-height: 500px;
 `;
 
-const Calendar = () => (
-  <Card body className="mt-3">
-    <Card.Title>Calendar</Card.Title>
-    <BigCalendar
-      localizer={localizer}
-      events={[]}
-      startAccessor="start"
-      endAccessor="end"
-    />
-  </Card>
-);
+interface ICalendar {
+  calendarId: string;
+  apiKey: string;
+}
+
+const getEvents = async (calendarId, apiKey) => {
+  const CURRENT_TIME = new Date().toISOString();
+  const CALENDAR_ID = calendarId;
+  const API_KEY = apiKey;
+  let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?timeMin=${CURRENT_TIME}&key=${API_KEY}`;
+  const response = await fetch(url);
+  const { items } = await response.json();
+  const events = items.map(event => ({
+    start: event.start.dateTime,
+    end: event.end.dateTime,
+    title: event.summary
+  }));
+  return events;
+};
+
+class Calendar extends Component<ICalendar> {
+  state = {
+    events: []
+  };
+
+  async componentDidMount() {
+    this.setState({
+      events: await getEvents(this.props.calendarId, this.props.apiKey)
+    });
+  }
+
+  render() {
+    return (
+      <Card body className="mt-3">
+        <Card.Title>Calendar</Card.Title>
+        <BigCalendar
+          localizer={localizer}
+          events={this.state.events}
+          startAccessor="start"
+          endAccessor="end"
+        />
+      </Card>
+    );
+  }
+}
 
 export { Calendar };
