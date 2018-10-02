5c60eede4abf1df6e40440abfa920f93059afede
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-10-02 19:08:30 +0200

    feat(QuickActions): Wrap in cards

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -2,8 +2,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 
 const Icon = styled(FontAwesomeIcon)`
-  margin-left: 0.25rem;
-  margin-right: 0.25rem;
+  margin-left: 0.24rem;
+  margin-right: 0.24rem;
 `;
 
 export { Icon };
