48896908d16641992f1084da34a8ef747aa95284
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-10-02 13:13:21 +0200

    feat: add links to external services

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
         short_name: "RvWG",
         start_url: "/",
         background_color: "#ffffff",
-        theme_color: "#ff7200",
+        theme_color: "#e0a800",
         display: "standalone",
         icon: "static/img/icon.png"
       }
