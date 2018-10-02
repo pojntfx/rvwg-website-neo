80cae49af71979207c3f32ad33c2c95e706ad22b
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-10-02 13:00:32 +0200

    feat: add quick actions

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { Button as ButtonTemplate } from "react-bootstrap";
+import { Icon } from "./Icon";
+
+const Button = ({ icon, content, ...otherProps }) => (
+  <ButtonTemplate variant="warning" {...otherProps}>
+    <Icon icon={icon} /> {content}
+  </ButtonTemplate>
+);
+
+export { Button };
