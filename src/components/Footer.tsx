659be3fe7ae4254a08cca7a61f957efe43818b92
Felix Pojtinger <felix@pojtinger.com>
2018-10-03 03:41:12 +0200

    feat: add mobile trigger in addition to konami code (swipe from footer to navbar), change background if easteregg has been triggered, add anti-cheat

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,8 @@ import { Navbar as NavbarTemplate, Nav, Container } from "react-bootstrap";
 import { Link } from "./Link";
 import { Icon } from "./Icon";
 import styled from "styled-components";
-import { EasterEgg } from "./EasterEgg";
+import { EasterEggTrigger } from "./EasterEggTrigger";
+import { Gesture } from "react-with-gesture";
 
 const Navbar = styled(NavbarTemplate)`
   overflow-x: auto;
@@ -23,19 +24,23 @@ interface IFooter {
 }
 
 const Footer = (props: IFooter) => (
-  <Navbar bg="light" variant="primary" className="mt-3">
-    <EasterEgg />
-    <Container className="px-0 px-sm-3">
-      <Nav className="ml-auto mr-auto">
-        {props.items.map(({ title, icon, to }, index) => (
-          <Nav.Link as={Link} to={to} key={index}>
-            <Icon icon={icon} />
-            <span>{title}</span>
-          </Nav.Link>
-        ))}
-      </Nav>
-    </Container>
-  </Navbar>
+  <Gesture>
+    {({ yDelta }) => (
+      <Navbar bg="light" variant="primary" className="mt-3">
+        <EasterEggTrigger yDelta={yDelta} />
+        <Container className="px-0 px-sm-3">
+          <Nav className="ml-auto mr-auto">
+            {props.items.map(({ title, icon, to }, index) => (
+              <Nav.Link as={Link} to={to} key={index}>
+                <Icon icon={icon} />
+                <span>{title}</span>
+              </Nav.Link>
+            ))}
+          </Nav>
+        </Container>
+      </Navbar>
+    )}
+  </Gesture>
 );
 
 export { Footer };
---
+++ src/pages/eastereggs.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { EasterEgg } from "../components/EasterEgg";
+import DefaultLayout from "../layouts/Default";
+import { Head } from "../components/Head";
+import { navigate } from "gatsby";
+
+const EasterEggs = ({ location }) => (
+  <DefaultLayout>
+    <Head title="Easter Eggs" />
+    {/* Double protection: Only show modal if location is given and if it is not, navigate back to home - 
+    this is necessary to prevent the background from appearing first! */}
+    {location.state ? (
+      location.state.easterEggIsVisible ? (
+        <EasterEgg
+          easterEggIsVisible={
+            location.state
+              ? location.state.easterEggIsVisible
+              : typeof window !== "undefined" && navigate("/")
+          }
+        />
+      ) : (
+        typeof window !== "undefined" && navigate("/")
+      )
+    ) : (
+      typeof window !== "undefined" && navigate("/")
+    )}
+  </DefaultLayout>
+);
+
+export default EasterEggs;
