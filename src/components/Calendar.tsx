4e4883afe670897ec9026dd57ad58a0a9d3c5400
Felicitas Pojtinger <felicitas@pojtinger.com>
2018-10-03 13:34:42 +0200

    feat(Calendar): add error handling

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { Component } from "react";
 import BigCalendarTemplate from "react-big-calendar";
-import { Card } from "react-bootstrap";
+import { Card, Alert } from "react-bootstrap";
 import styled from "styled-components";
 import { faCalendar } from "@fortawesome/free-solid-svg-icons";
 import { Icon } from "./Icon";
 import * as globalize from "../../.tmp-globalize-webpack/dev-i18n-data";
+import Fetch from "react-fetch-component";
 
 const localizer = BigCalendarTemplate.globalizeLocalizer(globalize);
 
@@ -21,13 +22,7 @@ interface ICalendar {
   title: string;
 }
 
-const getEvents = async (calendarId, apiKey) => {
-  const CURRENT_TIME = new Date().toISOString();
-  const CALENDAR_ID = calendarId;
-  const API_KEY = apiKey;
-  let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?timeMin=${CURRENT_TIME}&key=${API_KEY}`;
-  const response = await fetch(url);
-  const { items } = await response.json();
+const getEvents = items => {
   const events = items.map(event => ({
     start: new Date(event.start.dateTime) || new Date(event.start.date),
     end: new Date(event.end.dateTime) || new Date(event.end.date),
@@ -36,32 +31,67 @@ const getEvents = async (calendarId, apiKey) => {
   return events;
 };
 
+interface IExtendedError extends Error {
+  error: IExtendedNestedError;
+}
+
+interface IExtendedNestedError extends Error {
+  code: string;
+}
+
 class Calendar extends Component<ICalendar> {
-  state = {
-    events: []
-  };
-
-  async componentDidMount() {
-    this.setState({
-      events: await getEvents(this.props.calendarId, this.props.apiKey)
-    });
-  }
-
   render() {
+    const CURRENT_TIME = new Date().toISOString();
+    const CALENDAR_ID = this.props.calendarId;
+    const API_KEY = this.props.apiKey;
+    const URL = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?timeMin=${CURRENT_TIME}&key=${API_KEY}`;
+
     return (
       <Card body className="mt-3">
         <Card.Title>
           <Icon icon={faCalendar} />
           {this.props.title}
         </Card.Title>
-        <BigCalendar
-          defaultView="agenda"
-          views={["month", "agenda"]}
-          localizer={localizer}
-          events={this.state.events}
-          startAccessor="start"
-          endAccessor="end"
-        />
+        <Fetch url={URL}>
+          {({ loading, error, data }) => (
+            <div>
+              {loading && (
+                <Alert dismissible variant="info" className="mb-0">
+                  Kalenderdaten werden geladen ...
+                </Alert>
+              )}
+              {error && (
+                <Alert dismissible variant="danger" className="mb-0">
+                  <Alert.Heading>
+                    Oh nein! Es gab einen Fehler beim Laden der Kalender-Daten!
+                  </Alert.Heading>
+                  <i>
+                    Eventuell kann ihnen{" "}
+                    <a href="mailto:felicitas@pojtinger">Felicitas Pojtinger</a>, der
+                    Entwickler dieser Seite helfen - oder der folgende
+                    Fehlerbericht:
+                  </i>
+                  <br />
+                  <b>Fehlercode:</b> {(error as IExtendedError).error.code}
+                  <br />
+                  <b>Fehlernachricht:</b>{" "}
+                  {(error as IExtendedError).error.message}
+                </Alert>
+              )}
+              {data && (
+                <BigCalendar
+                  defaultView="agenda"
+                  views={["month", "agenda"]}
+                  localizer={localizer}
+                  events={getEvents(data.items)}
+                  startAccessor="start"
+                  endAccessor="end"
+                />
+              )}
+            </div>
+          )}
+        </Fetch>
+        {/* */}
       </Card>
     );
   }
